0113986adf0d5fb0e7cb0137a15bd472025a562e
jcolon030 <99977621+jcolon030@users.noreply.github.com>
2022-10-28 14:00:13 -0400

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final auth = FirebaseAuth.instanceFor(
+      app: Firebase.app(), persistence: Persistence.NONE);
   runApp(MyApp());
 }
 
